81c0b5ea45ee5c9b3c3ee53e933d321a533683bf
stephanie <swang93@mit.edu>
2016-11-29 01:34:53 -0800

    Matrix multiply block size as optional arg

--- workloads/mat_mult.py
+++ workloads/mat_mult.py
@@ -11,22 +11,26 @@ import ray.array.distributed as da
 parser = argparse.ArgumentParser(description="Run example of distributed matrix multipication.")
 parser.add_argument("--size", default=100, type=int, help="The size of the matrix")
 parser.add_argument("--workers", default=2, type=int, help="The number of Ray workers")
+parser.add_argument("--block-size", default=10, type=int, help="The matrix block size")
 
 
-def mat_mul(size, num_workers):
+def mat_mul(size, num_workers, block_size):
     # Start Ray.
     ray.init(start_ray_local=True, num_workers=num_workers)
     ray.register_class(da.DistArray)
     # Allocate two size x size arrays of all one's, and multiply them. Block on
     # the result.
-    print "Matrix multiply with size {}".format(size)
+    print "Matrix multiply with size {}, block size {}".format(size,
+            block_size)
     start = time.time()
-    a = da.ones.remote([size, size])
-    b = da.ones.remote([size, size])
+    a = da.ones.remote([size, size], block_size=block_size)
+    b = da.ones.remote([size, size], block_size=block_size)
     c = da.dot.remote(a, b)
     c = da.assemble.remote(c)
+    print c.id
     c_get = ray.get(c)
     end = time.time()
+    print c_get
     print "Took {} seconds".format(end - start)
     return ray
 
@@ -34,4 +38,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
     size = args.size
     num_workers = args.workers
-    mat_mul(size, num_workers)
+    block_size = args.block_size
+    mat_mul(size, num_workers, block_size)

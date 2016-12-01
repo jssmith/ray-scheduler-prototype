522ad74c772d50653606c3df8d1f89ab9208b593
Johann Schleier-Smith <jssmith@gmail.com>
2016-12-01 09:31:40 -0800

    rename comment to experiment_name, fix dump activity

--- sweep_dump_activity.py
+++ sweep_dump_activity.py
@@ -1,19 +1,45 @@
+import sys
 import ec2config
 
-def dump_activity():
+def usage():
+    print "Usage: sweep_dump_activity.py experiment_name"
+
+def dump_activity(experiment_name):
     sdb_conn = ec2config.sdb_connect()
     dom = sdb_conn.get_domain(ec2config.sdb_sweep_domain)
-    query = "select end_time from `{}` where end_time > '1480000000' and comment='jss-run-1' order by end_time desc".format(ec2config.sdb_sweep_domain)
+    query = "select * from `{}` where end_time > '1480000000' and (experiment_name='{}' or comment='{}') order by end_time desc".format(
+        ec2config.sdb_sweep_domain, experiment_name, experiment_name)
     rs = dom.select(query)
     end_times = []
+    elapsed_times = []
     for r in reversed(list(rs)):
         print r.name, r
-        end_times.append(float(r['end_time']))
-    print 'count', len(end_times)
-    print 'elapsed', max(end_times) - min(end_times), 's'
-    print 'elapsed', (max(end_times) - min(end_times)) / 3600, 'hr'
     sdb_conn.close()
 
+def measure_activity(experiment_name):
+    sdb_conn = ec2config.sdb_connect()
+    dom = sdb_conn.get_domain(ec2config.sdb_sweep_domain)
+    query = "select start_time, end_time from `{}` where end_time > '1480000000' and (experiment_name='{}' or comment='{}') order by end_time desc".format(
+        ec2config.sdb_sweep_domain, experiment_name, experiment_name)
+    rs = dom.select(query)
+    end_times = []
+    elapsed_times = []
+    for r in reversed(list(rs)):
+        end_time = float(r['end_time'])
+        start_time = float(r['start_time'])
+        elapsed_time = end_time - start_time
+        end_times.append(end_time)
+        elapsed_times.append(elapsed_time)
+    print '{} simulation run for experiment {}'.format(len(end_times), experiment_name)
+    elapsed_sec = max(end_times) - min(end_times)
+    print 'elapsed time {:.0f} seconds ({:.2f} hours)'.format(elapsed_sec, elapsed_sec/3600)
+    print 'avg simulation duartion {:.2f} seconds'.format(sum(elapsed_times) / float(len(elapsed_times)))
+    sdb_conn.close()
 
 if __name__ == '__main__':
-    dump_activity()
+    if len(sys.argv) != 2:
+        usage()
+        sys.exit(1)
+    experiment_name = sys.argv[1]
+    dump_activity(experiment_name)
+    measure_activity(experiment_name)
--- sweep_queue.py
+++ sweep_queue.py
@@ -6,7 +6,7 @@ import ec2config
 
 from boto.sqs.message import Message
 
-def enqueue(queue, num_nodes, scheduler, tracefile, comment):
+def enqueue(queue, num_nodes, scheduler, tracefile, experiment_name):
     m = Message()
     s = json.dumps({
             'scheduler': scheduler,
@@ -16,7 +16,7 @@ def enqueue(queue, num_nodes, scheduler, tracefile, comment):
             'db_message_delay': .001,
             'validate': 'false',
             'tracefile': str(tracefile),
-            'comment': str(comment)
+            'experiment_name': str(experiment_name)
         })
     print s
     m.set_body(s)
@@ -28,7 +28,7 @@ def queue_sweep(args):
     max_nodes = int(args[2])
     node_step_mult = float(args[3])
     schedulers = str.split(args[4])
-    comment = args[5]
+    experiment_name = args[5]
     tracefile = args[6]
 
     node_cts = []
@@ -48,12 +48,12 @@ def queue_sweep(args):
 
     for scheduler in schedulers:
         for num_nodes in node_cts:
-            enqueue(queue, num_nodes, scheduler, tracefile, comment)
+            enqueue(queue, num_nodes, scheduler, tracefile, experiment_name)
 
 
 def usage():
-    print 'Usage: queue_sweep.py min_nodes max_nodes node_step_mult schedulers comment tracefile'
-    print '    e.g., queue_sweep.py 1 16 2 "trivial location_aware" "just testing" trace.json'
+    print 'Usage: queue_sweep.py min_nodes max_nodes node_step_mult schedulers experiment_name tracefile'
+    print '    e.g., queue_sweep.py 1 16 2 "trivial location_aware" my-experiment-1 trace.json'
 
 if __name__ == '__main__':
     if len(sys.argv) >= 7:

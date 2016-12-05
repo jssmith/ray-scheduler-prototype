fde6ca60856fa188dd91a99e86a6ae39da44b510
Johann Schleier-Smith <jssmith@gmail.com>
2016-12-05 07:27:50 -0800

    poster figs output as png

--- poster_figs.py
+++ poster_figs.py
@@ -19,36 +19,36 @@ def poster_figs():
         'job_completion_time', 'Job Completion Time [seconds]',
         lambda x: not x['scheduler'].endswith('local'),
         'Global Schedulers - Synthetic Matrix Multiplication 16,000x16,000',
-        'poster_figs/syn_matmult_global.pdf')
+        'poster_figs/syn_matmult_global.png')
     drawplots_fn(experiment_name_graph2_matmult, lambda x: x['job_completion_time'],
         'job_completion_time', 'Job Completion Time [seconds]',
         lambda x: x['scheduler'].endswith('local'),
         'Local Schedulers - Synthetic Matrix Multiplication 16,000x16,000',
-        'poster_figs/syn_matmult_local.pdf')
+        'poster_figs/syn_matmult_local.png')
 
     experiment_name_graph2_rnn = 'graphs-dec4-e'
     drawplots_fn(experiment_name_graph2_rnn, lambda x: x['job_completion_time'],
         'job_completion_time', 'Job Completion Time [seconds]',
         lambda x: not x['scheduler'].endswith('local'),
         'Global Schedulers - RNN',
-        'poster_figs/rnn_global.pdf')
+        'poster_figs/rnn_global.png')
     drawplots_fn(experiment_name_graph2_rnn, lambda x: x['job_completion_time'],
         'job_completion_time', 'Job Completion Time [seconds]',
         lambda x: x['scheduler'].endswith('local'),
         'Local Schedulers - RLPong',
-        'poster_figs/rnn_local.pdf')
+        'poster_figs/rnn_local.png')
 
     experiment_name_graph2_rlpong = 'graphs-dec4-f'
     drawplots_fn(experiment_name_graph2_rlpong, lambda x: x['job_completion_time'],
         'job_completion_time', 'Job Completion Time [seconds]',
         lambda x: not x['scheduler'].endswith('local'),
         'Global Schedulers - RNN',
-        'poster_figs/rlpong_global.pdf')
+        'poster_figs/rlpong_global.png')
     drawplots_fn(experiment_name_graph2_rlpong, lambda x: x['job_completion_time'],
         'job_completion_time', 'Job Completion Time [seconds]',
         lambda x: x['scheduler'].endswith('local'),
         'Local Schedulers - RLPong',
-        'poster_figs/rlpong_local.pdf')
+        'poster_figs/rlpong_local.png')
 
     experiment_name_threshold = 'graphs-dec4-threshold-b'
     drawplots_generic(experiment_name_threshold,
@@ -56,7 +56,7 @@ def poster_figs():
         lambda x: x['job_completion_time'], 'job_completion_time', 'Job Completion Time [seconds]',
         lambda x: not x['scheduler'].startswith('trivial'),
         'Threshold - RLPong',
-        'poster_figs/rlpong_threshold.pdf')
+        'poster_figs/rlpong_threshold.png')
 
 
     # Figure 3
@@ -65,7 +65,7 @@ def poster_figs():
         'job_completion_time', 'Job Completion Time [seconds]',
         lambda x: x['scheduler'] in set(['trivial', 'trivial_local']),
         'Global vs Local - Synthetic Matrix Multiplication 16,000x16,000',
-        'poster_figs/syn_matmult_trivial_globalvlocal.pdf')
+        'poster_figs/syn_matmult_trivial_globalvlocal.png')
 
     experiment_name_graph3_matmult = experiment_name_graph2_matmult
     include_schedulers = set(['transfer_aware', 'transfer_aware_local',
@@ -75,14 +75,14 @@ def poster_figs():
         'job_completion_time', 'Job Completion Time [seconds]',
         lambda x: x['scheduler'] in include_schedulers,
         'Global vs Local - Synthetic Matrix Multiplication 16,000x16,000',
-        'poster_figs/syn_matmult_nontrivial_globalvlocal.pdf')
+        'poster_figs/syn_matmult_nontrivial_globalvlocal.png')
 
     experiment_name_graph3_rlpong = experiment_name_graph2_rlpong
     drawplots_fn(experiment_name_graph3_rlpong, lambda x: x['job_completion_time'],
         'job_completion_time', 'Job Completion Time [seconds]',
         lambda x: x['scheduler'] in set(['trivial', 'trivial_local']),
         'Global vs Local - RLPong',
-        'poster_figs/rlpong_trivial_globalvlocal.pdf')
+        'poster_figs/rlpong_trivial_globalvlocal.png')
 
     include_schedulers = set(['transfer_aware', 'transfer_aware_local',
         'transfer_aware_threshold_local', 'location_aware',
@@ -91,14 +91,14 @@ def poster_figs():
         'job_completion_time', 'Job Completion Time [seconds]',
         lambda x: x['scheduler'] in include_schedulers,
         'Global vs Local - RLPong',
-        'poster_figs/rlpong_nontrivial_globalvlocal.pdf')
+        'poster_figs/rlpong_nontrivial_globalvlocal.png')
 
     experiment_name_graph3_rnn = experiment_name_graph2_rlpong
     drawplots_fn(experiment_name_graph3_rnn, lambda x: x['job_completion_time'],
         'job_completion_time', 'Job Completion Time [seconds]',
         lambda x: x['scheduler'] in set(['trivial', 'trivial_local']),
         'Global vs Local - RNN',
-        'poster_figs/rnn_trivial_globalvlocal.pdf')
+        'poster_figs/rnn_trivial_globalvlocal.png')
 
     include_schedulers = set(['transfer_aware', 'transfer_aware_local',
         'transfer_aware_threshold_local', 'location_aware',
@@ -107,7 +107,7 @@ def poster_figs():
         'job_completion_time', 'Job Completion Time [seconds]',
         lambda x: x['scheduler'] in include_schedulers,
         'Global vs Local - RNN',
-        'poster_figs/rnn_nontrivial_globalvlocal.pdf')
+        'poster_figs/rnn_nontrivial_globalvlocal.png')
 
 
 if __name__ == '__main__':
